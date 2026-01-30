c9c07bd3b43a2cd23d7ca3b6e9f7d6ad3e7e5903
Alimi <alfikro21@gmail.com>
2026-01-30 07:30:37 +0700

    Update login_with_password.go

--- login_with_password.go
+++ login_with_password.go
@@ -1,18 +1,19 @@
+// --- LOGIN ---
 app.Post("/login", func(c *fiber.Ctx) error {
 		username := c.FormValue("username")
 		password := c.FormValue("password")
 
-		// Ambil hash dari DB berdasarkan username
-		storedHash, exists := userDatabase[username]
-		if !exists {
-			return c.Status(401).SendString("Username atau password salah")
+		var user User
+		// Cari user berdasarkan username di database
+		if err := DB.Where("username = ?", username).First(&user).Error; err != nil {
+			return c.Status(401).SendString("Username tidak ditemukan")
 		}
 
-		// Bandingkan hash dengan password yang diinput
-		err := bcrypt.CompareHashAndPassword([]byte(storedHash), []byte(password))
+		// Bandingkan password input dengan hash di DB
+		err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 		if err != nil {
-			return c.Status(401).SendString("Username atau password salah")
+			return c.Status(401).SendString("Password salah")
 		}
 
-		return c.SendString("Selamat datang, " + username + "!")
-})
+		return c.SendString("Login Berhasil! ID User Anda: " + fmt.Sprint(user.ID))
+	})
